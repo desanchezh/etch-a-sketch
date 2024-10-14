1e784fc4987aea6728526d3f71af2c6f45b050cd
desanchezh <129230744+desanchezh@users.noreply.github.com>
2024-10-14 08:43:16 -0400

    made square dimensions dynamic based on grid dimensions

--- etchASketch.js
+++ etchASketch.js
@@ -8,13 +8,24 @@ let square = document.createElement("div");
     square.style.borderColor = "black";
     square.style.backgroundColor = "red";
 
-for (i = 0; i < 256; i++ ) {
+
+gridDimensions = 80;
+
+//960px is the height/w of gridContainer in CSS, minus 1 for margin
+boxDimensions = 960 / gridDimensions - 1
+
+
+for (i = 0; i < (gridDimensions * gridDimensions); i++ ) {
     let square = document.createElement("div");
-    square.style.maxWidth = "59px";
-    square.style.maxHeight = "59px";
-    square.style.minHeight = "59px";
-    square.style.minWidth = "59px";
+    square.style.maxWidth = `${boxDimensions}px`;
+    square.style.maxHeight = `${boxDimensions}px`;
+    square.style.minHeight = `${boxDimensions}px`;
+    square.style.minWidth = `${boxDimensions}px`;
     square.style.margin = "0.5px"
     square.style.backgroundColor = "red";
     gridContainer.appendChild(square);
 }
+
+gridContainer.addEventListener("mouseover", function (e) {
+    e.target.style.backgroundColor = "black"
+})
