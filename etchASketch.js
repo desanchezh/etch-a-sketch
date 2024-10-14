34271a8a51a6549632464302019ac2ffe2ba41c3
desanchezh <129230744+desanchezh@users.noreply.github.com>
2024-10-14 09:39:17 -0400

    basic functionality complete pending extra credit

--- etchASketch.js
+++ etchASketch.js
@@ -1,31 +1,47 @@
 
 let gridContainer = document.querySelector ("#gridContainer");
-let square = document.createElement("div");
-    square.style.maxWidth = "60px";
-    square.style.maxHeight = "60px";
-    square.style.minHeight = "60px";
-    square.style.minWidth = "60px";
-    square.style.borderColor = "black";
-    square.style.backgroundColor = "red";
 
 
-gridDimensions = 80;
-
-//960px is the height/w of gridContainer in CSS, minus 1 for margin
+let gridDimensions = 16
+//960px is the height/w of gridContainer in CSS, minus 1 for square margin
 boxDimensions = 960 / gridDimensions - 1
+let dimensionsButton = document.querySelector("#dimensionsButton")
 
+addSquares(0, gridDimensions)
 
-for (i = 0; i < (gridDimensions * gridDimensions); i++ ) {
-    let square = document.createElement("div");
-    square.style.maxWidth = `${boxDimensions}px`;
-    square.style.maxHeight = `${boxDimensions}px`;
-    square.style.minHeight = `${boxDimensions}px`;
-    square.style.minWidth = `${boxDimensions}px`;
-    square.style.margin = "0.5px"
-    square.style.backgroundColor = "red";
-    gridContainer.appendChild(square);
+dimensionsButton.addEventListener("click", () => {
+    let dimension = prompt("Type dimension width(<100)", 16);
+    gridDimensions = parseInt(dimension)
+    if (gridDimensions > 100) {
+        alert ("100x100 is the maximum. Select a number < 100");
+        return;
+    }else{
+        removeSquares()
+        boxDimensions = 960 / gridDimensions - 1
+        addSquares(0, gridDimensions)
+        }
+    })
+    
+    
+
+function removeSquares() {
+        gridContainer.innerHTML=""
+}
+
+function addSquares(i, gridDimensions) {
+    for (i = 0; i < (gridDimensions * gridDimensions); i++ ) {
+        let square = document.createElement("div");
+        square.style.maxWidth = `${boxDimensions}px`;
+        square.style.maxHeight = `${boxDimensions}px`;
+        square.style.minHeight = `${boxDimensions}px`;
+        square.style.minWidth = `${boxDimensions}px`;
+        square.style.margin = "0.5px"
+        square.style.backgroundColor = "cornflowerblue";
+        gridContainer.appendChild(square);
+    }
 }
 
 gridContainer.addEventListener("mouseover", function (e) {
+    e.stopImmediatePropagation()
     e.target.style.backgroundColor = "black"
 })
